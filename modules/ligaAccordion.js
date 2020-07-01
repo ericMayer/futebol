52947b9c0172d878467aba8ccbb0b8964189114d
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-06-30 23:10:43 -0300

    Comentado código javascript

--- modules/ligaAccordion.js
+++ modules/ligaAccordion.js
@@ -5,15 +5,20 @@ export default class Accordion {
     this.descricao = document.querySelectorAll(descricao);
   }
 
+  // adicionando classe ativo ou removendo
+  // com isso será alterado a arrow de posição
+  // é também será exibido o conteúdo
   ativo(index) {
     this.ligas[index].classList.toggle("ativo");
     this.descricao[index].classList.toggle("ativo");
   }
 
+  // alterando referência do método de callback
   bind() {
     this.ativo = this.ativo.bind(this);
   }
 
+  // adicionando eventos nos itens
   addEvents() {
     this.arrow.forEach((item, index) => {
       item.addEventListener("click", () => {
@@ -22,6 +27,8 @@ export default class Accordion {
     });
   }
 
+  // inicia métodos para funcionamento do código
+  // e já coloca como ativo o primeiro item
   iniciar() {
     if (this.ligas.length && this.descricao.length && this.arrow.length) {
       this.bind();
